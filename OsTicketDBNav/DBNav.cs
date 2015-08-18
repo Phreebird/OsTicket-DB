7d5b4f0f7be3cbe5fce79974ab1480cb308e2795
Ryan Grimes <ryan.grimes@phreebird.com>
2015-08-17 21:50:31 -0500

    First Commit

---
+++ OsTicketDBNav/DBNav.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsTicketDBNav
+{
+    public class DBNav
+    {
+        public static void EstablishConnection(String server, String db_name, String user, String password)
+        {
+            //
+        }
+    }
+}
