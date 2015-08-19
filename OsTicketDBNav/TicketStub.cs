75edef8a67d1cb18f6b8f42efa408c6ac6dcc10d
Ryan Grimes <ryan.grimes@phreebird.com>
2015-08-19 14:32:33 -0500

    Ticket Reorginization
    Added ticketstub.cs

---
+++ OsTicketDBNav/TicketStub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsTicketDBNav
+{
+    class TicketStub
+    {
+        public TicketStub() { }
+        public String toString()
+        {
+            return "";
+        }
+        //TODO: Add sophisticaed constructor
+        public String ticketSubject { get; set; }
+        public int ticketNumber { get; set; }
+        public int ticketTrueNumber { get; set; }
+        public String creatorUserName { get; set; }
+        public String creatorIpAddress { get; set; }
+        public int ticketPriority { get; set; }
+        public Boolean isOpen { get; set; }
+        public String ticketCompany { get; set; }
+        public DateTime ticketCreationDate { get; set; }
+        public DateTime ticketLastResponse { get; set; }
+    }
+}
